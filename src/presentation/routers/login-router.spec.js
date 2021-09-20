05ed135279c04bd1fb13e7bd09379a4d6cc1f99d
Mateus0808 <loiolamateus7@gmail.com>
2021-09-20 10:07:39 -0300

    ensure LoginRouter returns 400 if no email is provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,24 @@
+const { describe, test, expect } = require('@jest/globals')
+
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: '1234'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
